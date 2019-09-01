af86549321f0e2c4407319554324856d511699d1
Sergio Bugallo Enjamio <dnamaster.22@gmail.com>
2019-09-01 18:19:58 +0200

    Update docs

--- gridworld/models/state.py
+++ gridworld/models/state.py
@@ -10,33 +10,19 @@ class State:
     cell_id: int
     cell_type: int
     actions: dict
-        Format: {
-            gridworld.models.Action: {
-                "cell_id": int,
-                "reward": gridworld.models.Reward,
-                "transition_probability": float,
-                "is_goal": bool
-            }
-        }
     """
 
     def __init__(self, cell_id: int, possible_moves: dict, cell_type: int) -> None:
         """
+
         Parameters
         ----------
         cell_id: int
             Cell index in world's grid.
         possible_moves: dict
-            Format: {
-                gridworld.models.Action: {
-                    "cell_id": int,
-                    "reward": gridworld.models.Reward,
-                    "transition_probability": float,
-                    "is_goal": bool
-                }
-            }
         cell_type: int
             0 -> road, 1 -> start, 2 -> goal, -1 -> obstacle
+
         """
         self.cell_id = cell_id
         self.cell_type = cell_type
@@ -64,11 +50,7 @@ class State:
         Returns
         -------
         results: dict
-            Format: {
-                "cell_id": int,
-                "reward": gridworld.models.Reward,
-                "transition_probability": float,
-                "is_goal": bool
-            }
+            Format: {cell_id: int, reward: gridworld.models.Reward, transition_probability: float, is_goal: bool}
         """
+
         return self.actions[action]
